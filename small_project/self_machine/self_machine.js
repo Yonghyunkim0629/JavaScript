fbc3a897f63b39fd310282af3770b90031531854
Yonghyunkim0629 <ky8175@gmail.com>
2021-10-28 14:34:45 -0400

    asdf

---
+++ small_project/self_machine/self_machine.js
@@ -0,0 +1,8 @@
+// let priceOfItem1, priceOfItem2, priceOfItem3;
+// let quantityOfItem1, quantityOfItem2, quantityOfItem;
+
+// let subTotal, tax, total;
+
+// let btn = document.getElementById("item--btn");
+// let inputItem = document.getElementsByClassName("nput--items");
+// inputItem.append(btn);
